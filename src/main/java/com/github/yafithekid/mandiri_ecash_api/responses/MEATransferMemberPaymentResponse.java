7e154509221f8ebd5d8d3d6a565c69960dd1f78e
Muhammad Yafi <yafithekid212@gmail.com>
2016-02-10 10:49:59 +0700

    fill empty class MEATransferMemberPaymentResponse

--- src/main/java/com/github/yafithekid/mandiri_ecash_api/responses/MEATransferMemberPaymentResponse.java
+++ src/main/java/com/github/yafithekid/mandiri_ecash_api/responses/MEATransferMemberPaymentResponse.java
@@ -1,6 +1,49 @@
 package com.github.yafithekid.mandiri_ecash_api.responses;
 
 public class MEATransferMemberPaymentResponse {
+    String toAccount;
+    String amount;
+    String trxDate;
+    String status;
+    String fromAccount;
 
+    public String getToAccount() {
+        return toAccount;
+    }
 
+    public void setToAccount(String toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getTrxDate() {
+        return trxDate;
+    }
+
+    public void setTrxDate(String trxDate) {
+        this.trxDate = trxDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(String fromAccount) {
+        this.fromAccount = fromAccount;
+    }
 }
